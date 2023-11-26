1a9606df798086554d82991b43541b7c4796a769
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-11-26 16:44:42 +0200

    refactor: deleted routes that are not in the file with routes

--- controllers/orders/index.js
+++ controllers/orders/index.js
@@ -1,7 +1,2 @@
 export * from './get-all/index.js';
 export * from './create/index.js';
-export * from './get-by-chef-id/index.js';
-export * from './get-by-courier-id/index.js';
-export * from './get-by-id/index.js';
-export * from './get-by-user-id/index.js';
-export * as changeStatus from './change-status/index.js';
